caecce15a627c43398993601625723cc3e73f258
Mike Way <mikeway@webwrights.co.uk>
2016-07-27 10:39:22 +0100

    Cleaned up excess files

--- Samples/Thursday/e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class ThursdayPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
-}
--- Samples/five_mins/app/passengers/passenger-list.module.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-// Define the `phoneList` module
-angular.module('passengerList', []);
-//# sourceMappingURL=passenger-list.module.js.map
