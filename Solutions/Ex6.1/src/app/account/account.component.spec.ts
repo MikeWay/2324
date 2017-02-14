1e3963b75ba570266d561c8fbb448a8b68ad2812
= <=>
2017-02-14 19:30:57 +0000

    Done to end Ex6.1.

--- Solutions/Ex6.1/src/app/account/account.component.spec.ts
+++ Solutions/Ex6.1/src/app/account/account.component.spec.ts
@@ -1,11 +1,28 @@
 /* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-import { TestBed, async } from '@angular/core/testing';
 import { AccountComponent } from './account.component';
 
-describe('Component: Account', () => {
-  it('should create an instance', () => {
-    let component = new AccountComponent();
+describe('AccountComponent', () => {
+  let component: AccountComponent;
+  let fixture: ComponentFixture<AccountComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AccountComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AccountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
