bf7eca6d0086cde48b7557a30083fd8d448677bf
= <=>
2017-02-14 18:26:00 +0000

    Done to end of Ex3.1

--- Solutions/Ex2.1_Bonus/src/app/app.component.spec.ts
+++ Solutions/Ex2.1_Bonus/src/app/app.component.spec.ts
@@ -2,32 +2,34 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {HomeComponent} from "./home/home.component";
 
-describe('App: FlySharp', () => {
+describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, HomeComponent
       ],
     });
+    TestBed.compileComponents();
   });
 
   it('should create the app', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'Welcome to Fly Sharp'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Welcome to Fly Sharp');
   }));
 
   it('should render title in a h1 tag', async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    let compiled = fixture.debugElement.nativeElement;
+    const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to Fly Sharp');
   }));
 });
--- Solutions/Ex2.2_Bonus/src/app/buy-flight/buy-flight.component.spec.ts
+++ Solutions/Ex2.2_Bonus/src/app/buy-flight/buy-flight.component.spec.ts
@@ -1,11 +1,28 @@
 /* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-import { TestBed, async } from '@angular/core/testing';
 import { BuyFlightComponent } from './buy-flight.component';
 
-describe('Component: BuyFlight', () => {
-  it('should create an instance', () => {
-    let component = new BuyFlightComponent();
+describe('BuyFlightComponent', () => {
+  let component: BuyFlightComponent;
+  let fixture: ComponentFixture<BuyFlightComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BuyFlightComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BuyFlightComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
