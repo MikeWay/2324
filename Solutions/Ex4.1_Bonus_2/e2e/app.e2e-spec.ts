4c03723be065da36e7f29beacdb6525f5c759940
Mike Way <mikeway@webwrights.co.uk>
2017-02-15 14:14:57 +0000

    Fixed start point for Ex4.2

--- Solutions/Ex4.1_Bonus_2/e2e/app.e2e-spec.ts
+++ Solutions/Ex4.1_Bonus_2/e2e/app.e2e-spec.ts
@@ -7,20 +7,10 @@ describe('fly-sharp App', function() {
     page = new FlySharpPage();
   });
 
-  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
+  it('should display message saying app works!', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
-  });
-
-  it('should show 5 rows in the table', () => {
-    page.navigateTo();
-    expect(page.getNumTableRows()).toEqual(5);
+    expect(page.getParagraphText()).toEqual('app works');
   });
 
 
-  it('should show 0 rows in the table when toggle is clicked', () => {
-    page.navigateTo();
-    page.clickToggle();
-    expect(page.getNumTableRows()).toEqual(0);
-  });
 });
--- Solutions/Ex4.1_Bonus_2/e2e/app.po.ts
+++ Solutions/Ex4.1_Bonus_2/e2e/app.po.ts
@@ -9,11 +9,5 @@ export class FlySharpPage {
     return element(by.css('app-root h1')).getText();
   }
 
-  getNumTableRows() {
-    return (element.all(by.css('table tbody tr'))).count();
-  }
 
-  clickToggle() {
-    element(by.css('#toggle')).click();
-  }
 }
