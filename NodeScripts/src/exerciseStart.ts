fd4c38ceee4376f9c7c327f38212e3b75158d46e
Mike Way <mikeway@webwrights.co.uk>
2017-02-20 10:57:56 -0800

    Modified mappings for ex start

--- NodeScripts/src/exerciseStart.ts
+++ NodeScripts/src/exerciseStart.ts
@@ -2,19 +2,19 @@
 var fs = requirejs('fs-extra')
 
 const EX_MAPPINGS = {
-	"Ex2.2" : "Ex2.1_Bonus",
-	"Ex3.1" : "Ex2.2_Bonus_2",
-	"Ex4.1" : "Ex3.1",
-	"Ex4.2" : "Ex4.1_Bonus_2",
-	"Ex5.1" : "Ex4.2",
-	"Ex5.2" : "Ex5.1_Bonus_1",
-	"Ex6.1" : "Ex5.2_Bonus_3",
-	"Ex6.2" : "Ex6.1_Bonus_2",
-	"Ex7.1" : "Ex6.2_Bonus",
-	"Ex7.2" : "Ex7.1_Bonus",
-	"Ex8.1" : "Ex7.2_Bonus",
-	"Ex9.1" : "Ex8.1_Bonus_1"
-	};
+    "Ex2.2": "Ex2.1_Bonus",
+    "Ex3.1": "Ex2.2_Bonus_2",
+    "Ex4.1": "Ex3.1",
+    "Ex4.2": "Ex4.1_Bonus_2",
+    "Ex5.1": "Ex4.2",
+    "Ex5.2" : "Ex5.1_Bonus_1",
+    "Ex6.1": "Ex5.2_Bonus_3",
+    "Ex6.2": "Ex6.1_Bonus_2",
+    "Ex7.1": "Ex6.2_Bonus_3",
+    "Ex7.2": "Ex7.1_Bonus_1",
+    "Ex8.1": "Ex7.2_Bonus_1",
+    "Ex9.1": "Ex8.1_Bonus_1"
+};
 
 /*
 Ex2.1_Bonus
