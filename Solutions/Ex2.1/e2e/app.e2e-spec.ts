76edad978aac86e767457634dd369c7624b935e4
Mike Way <mikeway@webwrights.co.uk>
2017-02-24 08:37:16 +0000

    Added e2e course testing up to start Ex3.1

--- Solutions/Ex2.1/e2e/app.e2e-spec.ts
+++ Solutions/Ex2.1/e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('fly-sharp App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to Fly Sharp');
   });
 });
---
+++ Solutions/Ex2.2_Bonus_2/e2e/course-check.e2e-spec.ts
@@ -0,0 +1,48 @@
+import {FlySharpCourseCheckPage} from './course-check.po';
+
+describe('Validate exercise 3.1 start', function() {
+  let page: FlySharpCourseCheckPage;
+
+  beforeEach(() => {
+    page = new FlySharpCourseCheckPage();
+  });
+
+  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
+  });
+
+  it('should have an App-Home component', () => {
+    page.navigateTo();
+    expect(page.getAppHomeH1()).toEqual('Special Offer of the month 10% off all round-the-World flights');
+  });  
+
+  it('should have a nav element', () => {
+    page.navigateTo();
+    expect(page.getNavBar().isPresent()).toBeTruthy();
+  });   
+
+  it('should have an app-buy-flights element', () => {
+    page.navigateTo();
+    expect(page.getBuyFlightsElement().isPresent()).toBeTruthy();
+  });      
+  it('should have a Toggle Flights button', () => {
+    page.navigateTo();
+
+    expect(page.getToggleFlightsButtonText()).toEqual("Toggle Flights");
+  });    
+
+  it('should have a 0 flights displayed', () => {
+    page.navigateTo();
+
+    expect(page.getFlightTableRows()).toBe(0);
+  });    
+
+  it('should have a 3 flights displayed', () => {
+    page.navigateTo();
+    page.clickToggleFlights();
+    expect(page.getFlightTableRows()).toBe(4);
+  });      
+
+
+});
