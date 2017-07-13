b1f62d806759bd339babf38df6c7e8d40a89fcc1
Mike Way <mikeway@webwrights.co.uk>
2017-07-13 14:25:46 +0100

    Unsolved DoNow21

--- DoNows/DoNow21/src/app/app.module.ts
+++ DoNows/DoNow21/src/app/app.module.ts
@@ -2,11 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ForecastComponent } from './forecast';
 
 @NgModule({
   declarations: [
-    AppComponent, ForecastComponent
+    AppComponent 
   ],
   imports: [
     BrowserModule
