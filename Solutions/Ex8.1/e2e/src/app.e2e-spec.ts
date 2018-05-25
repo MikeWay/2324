cd2a6bff0f1e127064126201a4b58b05154ba8bc
mikeway <mikeway@webwrights.co.uk>
2018-05-25 08:14:19 -0700

    Moved e2e tests to src dir, replaced missing routes.ts, removed router-stubs.ts

--- Solutions/Ex8.1/e2e/src/app.e2e-spec.ts
+++ Solutions/Ex8.1/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,41 @@
-import { AppPage } from './app.po';
+import { FlySharpPage } from './app.po';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+describe('fly-sharp App', function() {
+  let page: FlySharpPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new FlySharpPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
   });
+
+  it('should show 5 rows in the table', () => {
+    page.navigateToTab('buy');
+    expect(page.getNumTableRows()).toEqual(5);
+  });
+
+
+  it('should show 0 rows in the table when toggle is clicked', () => {
+    page.navigateToTab('buy');
+    page.clickToggle();
+    expect(page.getNumTableRows()).toEqual(0);
+  });
+
+  it('should show 9 columns in the table', () => {
+    page.navigateToTab('buy');
+    expect(page.getNumTableCols()).toEqual(9);
+  });  
+
+  it('flight number for 5th flight should be FS2211', () => {
+    page.navigateToTab('buy');
+    expect(page.getTableCellData('5','2')).toBe("FS2211");
+  })
+
+  it('destination for 5th flight should be LHR', () => {
+    page.navigateToTab('buy');
+    expect(page.getTableCellData('5','4')).toBe("LHR");
+  })  
 });
