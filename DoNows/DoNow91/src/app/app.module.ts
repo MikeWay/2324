15549ac95607660ebccfea375749a88cd89a465f
Mike Way <mikeway@webwrights.co.uk>
2018-06-13 10:24:31 +0100

    DoNows reworked up to end of DN51

--- DoNows/DoNow91/src/app/app.module.ts
+++ DoNows/DoNow91/src/app/app.module.ts
@@ -1,20 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
 import { AppComponent } from './app.component';
-import {PreferencesFormComponent} from "./preferences-form/preferences-form.component";
-import {BrowserModule} from "@angular/platform-browser";
-import {FormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
-import {NgModule} from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent, PreferencesFormComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
