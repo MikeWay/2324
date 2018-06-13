85f856c31e14265782200ac03e296189b4be53e0
Mike Way <mikeway@webwrights.co.uk>
2018-06-13 11:35:49 +0100

    DoNows reworked

--- DoNows/DoNow91/src/app/app.module.ts
+++ DoNows/DoNow91/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {PreferencesFormComponent} from "./preferences-form/preferences-form.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {NgModule} from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, PreferencesFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ DoNows/DoNow91/src/app/preferences-form/preferences-form.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit } from '@angular/core';
+
+import { Preferences } from '../entities/preferences'
+import {state, transition, trigger, animate, style } from "@angular/animations";
+
+@Component({
+  selector: 'app-preferences-form',
+  templateUrl: 'preferences-form.component.html',
+  styleUrls: ['preferences-form.component.css'],
+  animations: [
+    trigger('myanimate', [
+      state('on', style({
+        opacity : 1
+      })),
+      state('off',   style({
+        opacity : 0
+      })),
+      transition('on => off', animate('1000ms ease-out')),
+      transition('off => on', animate('1000ms ease-in'))
+    ])]
+  })
+export class PreferencesFormComponent implements OnInit {
+
+  temperatureUnits = ['F', 'C', 'K'];
+  speedUnits = ['Knots', 'MPH', 'KPH' , 'M/S'];
+  onOff = "on";
+
+  model = new Preferences(  "Long John Silver", "Treasure Island", "F", "Knots", 10 );
+
+
+  ngOnInit() {
+  }
+
+
+  get jsonData() { return JSON.stringify(this.model); }
+
+  onShow(){
+    this.onOff = "on";
+  }
+
+  onHide(){
+    this.onOff = "off";
+  }
+}
