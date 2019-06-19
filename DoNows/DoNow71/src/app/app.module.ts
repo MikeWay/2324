0cfd5580459dcf1ae144eda24405a3993f326db7
Mike Way <mikeway@webwrights.co.uk>
2019-06-19 17:37:23 +0100

    Updated DoNow 71 for A8

--- DoNows/DoNow71/src/app/app.module.ts
+++ DoNows/DoNow71/src/app/app.module.ts
@@ -1,19 +1,16 @@
+import { PreferencesFormComponent } from './preferences-form/preferences-form.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import {PreferencesFormComponent} from "./preferences-form/preferences-form.component";
 
 @NgModule({
   declarations: [
-    AppComponent,PreferencesFormComponent
+    AppComponent, PreferencesFormComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DoNows/DoNow71/src/app/preferences-form/preferences-form.component.ts
+++ DoNows/DoNow71/src/app/preferences-form/preferences-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Preferences } from '../entities/preferences';
 
-import { Preferences } from '../entities/preferences'
 
 @Component({
   selector: 'app-preferences-form',
