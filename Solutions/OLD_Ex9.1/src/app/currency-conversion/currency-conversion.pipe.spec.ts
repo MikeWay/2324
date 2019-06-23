cbac211876669377a499e73574ca7f91489a4b41
Mike Way <mikeway@webwrights.co.uk>
2019-06-23 16:45:53 +0100

    Removed old exercise solutions

--- Solutions/OLD_Ex9.1/src/app/currency-conversion/currency-conversion.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CurrencyConversionPipe } from './currency-conversion.pipe';
-
-describe('CurrencyConversionPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CurrencyConversionPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
