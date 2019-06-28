500df86c333080eb49a5690ad6d4b475b873d37f
mikeway <mikeway@webwrights.co.uk>
2019-06-28 02:54:45 -0700

    Roled back move to IVY due to bugs with IVY

---
+++ DoNows/updateNodeModules.bat
@@ -0,0 +1,32 @@
+IF "%selfWrapped%"=="" (
+  REM this is necessary so that we can use "exit" to terminate the batch file,
+  REM and all subroutines, but not the original cmd.exe
+  SET selfWrapped=true
+  %ComSpec% /s /c ""%~0" %*"
+  GOTO :EOF
+)
+
+cls
+echo "Make sure you have met the pre-reqs. See $COURSE_HOME/testing.md"
+echo "Starting tests"
+set COURSE_HOME=C:\course2324
+cd %COURSE_HOME%\Exercises
+
+
+FOR %%E in ("DoNow22", "DoNow41", "DoNow51", "DoNow71","DoNow91") DO (
+CALL :run_donow_update %%E
+IF %ERRORLEVEL% NEQ 0 Exit 1
+)
+
+exit /B
+
+:run_donow_update
+cd %COURSE_HOME%\DoNows\%1
+rmdir /s /q node_modules 
+call npm install
+IF %ERRORLEVEL% NEQ 0 Echo An error was found when updating node modules
+EXIT /B
+
+
+
+
