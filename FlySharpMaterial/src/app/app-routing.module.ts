953ca0a362bafc1b9d5e9583d75ca81cd4baaf57
mikeway <mikeway@webwrights.co.uk>
2019-07-03 01:54:01 -0700

    Initial revision of revised FlySharpMaterial example for A8 (not working)

---
+++ FlySharpMaterial/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BuyFlightMatComponent } from './buy-flight-mat/buy-flight-mat.component';
+import { BuyFlightComponent } from './buy-flight/buy-flight.component';
+import { MyFlightsComponent } from './my-flights/my-flights.component';
+import { AccountComponent } from './account/account.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'buy',
+    component: BuyFlightMatComponent,
+    pathMatch: 'prefix'
+  },
+  {
+    path: 'buy/:origin',
+    component: BuyFlightComponent
+  },
+  {
+    path: 'myflights',
+    component: MyFlightsComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FlySharpMaterial/src/app/model/account.ts
@@ -0,0 +1,10 @@
+export interface Account {
+  firstName: string,
+  familyName: string,
+  email: string,
+  address1: string,
+  address2: string,
+  city: string,
+  postCode: string
+}
+ 
