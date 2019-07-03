1368fb10b9ae5e5985880127c832115fe9e0797b
mikeway <mikeway@webwrights.co.uk>
2019-07-03 02:06:29 -0700

    Renamed 2.2Bonus_2 as Bonus_3

---
+++ Solutions/Ex2.2_Bonus_3/e2e/src/app.e2e-spec.ts
@@ -0,0 +1,23 @@
+import { AppPage } from './app.po';
+import { browser, logging } from 'protractor';
+
+describe('workspace-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
+    page.navigateTo();
+    expect(page.getTitleText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
+  });
+
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
+  });
+});
---
+++ Solutions/Ex2.2_Bonus_3/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { HomeComponent } from './home/home.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BuyFlightComponent } from './buy-flight/buy-flight.component';
+
+@NgModule({
+  declarations: [
+    AppComponent, HomeComponent, BuyFlightComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
