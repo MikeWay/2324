828b4e51aa315e6b93c52aa0ffc8d31886101e29
mikejrway <mikeway@webwrights.co.uk>
2020-12-20 18:02:12 +0000

    Added code for DoNow 42

--- DoNows/DoNow41/src/app/app.component.spec.ts
+++ DoNows/DoNow41/src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+
+@Component({
+  selector: 'app-current-weather',
+  template:''
+})
+class MockWeatherComponent {
+  
+}
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, MockWeatherComponent 
       ],
     }).compileComponents();
   });
