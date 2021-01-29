93e654a4977b519e85c5558048246e9465cf341d
Mike Way <mikeway@webwrights.co.uk>
2021-01-29 12:50:38 +0000

    Fixed garbage chars in exerciseStart.ts

--- NodeScripts/src/exerciseStart.ts
+++ NodeScripts/src/exerciseStart.ts
@@ -6,19 +6,19 @@ import * as fs from "fs-extra";
 
 
 const EX_MAPPINGS = {
-    "Ex2.2": "Ex2.1_Bonus", OK
-    "Ex3.1": "Ex2.2_Bonus_3", OK
-    "Ex4.1": "Ex3.1", OK
-    "Ex4.2": "Ex4.1_Bonus_2", OK
-    "Ex5.1": "Ex4.2_Bonus_1", OK
-    "Ex5.2" : "Ex5.1_Bonus_1", OK
-    "Ex6.1": "Ex5.2_Bonus_3", OK
-	"Ex6.2": "Ex6.1_Bonus_2", OK
-	"Ex6.3": "Ex6.2", OK
-    "Ex7.1": "Ex6.3_Bonus_3", OK
-    "Ex7.2": "Ex7.1_Bonus_1", OK
-    "Ex8.1": "Ex7.2_Bonus_1", OK
-	"Ex8.2": "Ex8.1_Bonus_2", IN - progress -- need fix and lint
+    "Ex2.2": "Ex2.1_Bonus",
+    "Ex3.1": "Ex2.2_Bonus_3",
+    "Ex4.1": "Ex3.1",
+    "Ex4.2": "Ex4.1_Bonus_2",
+    "Ex5.1": "Ex4.2_Bonus_1",
+    "Ex5.2" : "Ex5.1_Bonus_1",
+    "Ex6.1": "Ex5.2_Bonus_3",
+	"Ex6.2": "Ex6.1_Bonus_2",
+	"Ex6.3": "Ex6.2",
+    "Ex7.1": "Ex6.3_Bonus_3",
+    "Ex7.2": "Ex7.1_Bonus_1",
+    "Ex8.1": "Ex7.2_Bonus_1",
+	"Ex8.2": "Ex8.1_Bonus_2",
 	"Ex9.1": "Ex8.2",
 	"Final": "Ex9.1_Bonus_1"
 };
