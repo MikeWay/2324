750d5fab39d9f89316f72ad1eb1e39a12de8ea64
Mike Way <mikeway@webwrights.co.uk>
2021-01-30 17:05:53 +0000

    Ex3.1

--- Solutions/Ex3.1/e2e/src/course-check.e2e-spec.ts
+++ Solutions/Ex3.1/e2e/src/course-check.e2e-spec.ts
@@ -1,7 +1,7 @@
 import {FlySharpCourseCheckPage} from './course-check.po';
 import { browser, logging } from 'protractor';
 
-describe('Validate exercise 4.1 start', () => {
+describe('Validate exercise 2.2 start', () => {
   let page: FlySharpCourseCheckPage;
 
   beforeEach(() => {
@@ -9,40 +9,9 @@ describe('Validate exercise 4.1 start', () => {
   });
 
 
-  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
-  });
-  it('should have an App-Home component', () => {
-    page.navigateTo();
-    expect(page.getAppHomeH1()).toEqual('Special Offer of the month 10% off all round-the-World flights');
-  });
-
-  it('should have a nav element', () => {
-    page.navigateTo();
-    expect(page.getNavBar().isPresent()).toBeTruthy();
-  });
-
-  it('should have an app-buy-flights element', () => {
-    page.navigateTo();
-    expect(page.getBuyFlightsElement().isPresent()).toBeTruthy();
-  });
-  it('should have a Toggle Flights button', () => {
-    page.navigateTo();
-
-    expect(page.getToggleFlightsButtonText()).toEqual('Toggle Flights');
-  });
-
-  it('should have a 0 flights displayed', () => {
-    page.navigateTo();
-    page.clickToggleFlights();
-
-    expect(page.getFlightTableRows()).toBe(0);
-  });
-
-  it('should have a 5 flights displayed', () => {
-    page.navigateTo();
-    expect(page.getFlightTableRows()).toBe(6);
+  it('should have an App-Home component', async () => {
+    await page.navigateTo();
+    expect(await page.getAppHomeH1()).toEqual('Special Offer of the month 10% off all round-the-World flights');
   });
 
   afterEach(async () => {
--- Solutions/Ex3.1/e2e/src/course-check.po.ts
+++ Solutions/Ex3.1/e2e/src/course-check.po.ts
@@ -1,36 +1,16 @@
 import { browser, element, by } from 'protractor';
 
 export class FlySharpCourseCheckPage {
-  navigateTo() {
-    return browser.get('/');
+
+  async navigateTo(): Promise<unknown> {
+    return browser.get(browser.baseUrl);
   }
 
-  getParagraphText() {
+  async getParagraphText(): Promise<string> {
     return element(by.css('app-root h1')).getText();
   }
 
-  getAppHomeH1() {
+  async getAppHomeH1(): Promise<string>{
     return element(by.css('app-home h1')).getText();
   }
-
-  getNavBar() {
-    return element(by.css('app-root nav'));
-  }
-
-  getBuyFlightsElement() {
-    return element(by.css('app-root app-buy-flight'));
-  }
-
-  getToggleFlightsButtonText() {
-    return element(by.css('app-root app-buy-flight a')).getText();
-  }
-
-  clickToggleFlights() {
-    element(by.css('app-root app-buy-flight a')).click();
-  }
-
-  getFlightTableRows() {
-    return (element.all(by.css('app-buy-flight table tr'))).count();
-  }
-
 }
