a30ebf4578818fa245ebc0e23181aa21694d2569
Mike Way <mikeway@webwrights.co.uk>
2021-01-30 13:11:16 +0000

    Ex2.2_Bonus_2

--- Solutions/Ex2.2_Bonus_2/e2e/src/app.e2e-spec.ts
+++ Solutions/Ex2.2_Bonus_2/e2e/src/app.e2e-spec.ts
@@ -1,9 +1,6 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-/**
- * The tests will all fail because of the AppTime directive -- the every changing clock stops Protractor seeing the page as stable!
- */
 describe('workspace-project App', () => {
   let page: AppPage;
 
@@ -11,34 +8,16 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('FlySharp app is running!');
   });
 
-  it('should show 20 rows in the table', () => {
-    page.navigateToTab('buy');
-    expect(page.getNumTableRows()).toEqual(20);
-  });
-
-  it('should show 0 rows in the table when toggle is clicked', () => {
-    page.navigateToTab('buy');
-    page.clickToggle();
-    expect(page.getNumTableRows()).toEqual(0);
-  });
-
-  it('should show 9 columns in the table', () => {
-    page.navigateToTab('buy');
-    expect(page.getNumTableCols()).toEqual(9);
-  });
-
-  it('flight number for 5th flight should be 114', () => {
-    page.navigateToTab('buy');
-    expect(page.getTableCellData('5', '2')).toBe('114');
-  });
-
-  it('destination for 5th flight should be JFK', () => {
-    page.navigateToTab('buy');
-    expect(page.getTableCellData('5', '4')).toBe('JFK');
+  afterEach(async () => {
+    // Assert that there are no errors emitted from the browser
+    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+    expect(logs).not.toContain(jasmine.objectContaining({
+      level: logging.Level.SEVERE,
+    } as logging.Entry));
   });
 });
