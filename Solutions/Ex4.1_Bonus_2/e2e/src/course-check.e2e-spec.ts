f1cc4c7bc7b81c840439b9050643af874c4cea3a
Mike Way <mikeway@webwrights.co.uk>
2021-01-31 12:29:21 +0000

    Ex4.1_Bonus_2

--- Solutions/Ex4.1_Bonus_2/e2e/src/course-check.e2e-spec.ts
+++ Solutions/Ex4.1_Bonus_2/e2e/src/course-check.e2e-spec.ts
@@ -1,7 +1,7 @@
 import {FlySharpCourseCheckPage} from './course-check.po';
 import { browser, logging } from 'protractor';
 
-describe('Validate exercise 4.2 start', () => {
+describe('Validate exercise 4.1 start', () => {
   let page: FlySharpCourseCheckPage;
 
   beforeEach(() => {
@@ -9,40 +9,41 @@ describe('Validate exercise 4.2 start', () => {
   });
 
 
-  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
+  it('should display message saying Special Offer of the month 10% off all round-the-World flights', async () => {
+    await page.navigateTo();
+    expect(await page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
   });
-  it('should have an App-Home component', () => {
-    page.navigateTo();
-    expect(page.getAppHomeH1()).toEqual('Special Offer of the month 10% off all round-the-World flights');
-  });
-
-  it('should have a nav element', () => {
-    page.navigateTo();
-    expect(page.getNavBar().isPresent()).toBeTruthy();
+  it('should have an App-Home component', async () => {
+    await page.navigateTo();
+    expect(await page.getAppHomeH1()).toEqual('Special Offer of the month 10% off all round-the-World flights');
   });
 
-  it('should have an app-buy-flights element', () => {
-    page.navigateTo();
-    expect(page.getBuyFlightsElement().isPresent()).toBeTruthy();
-  });
-  it('should have a Toggle Flights button', () => {
-    page.navigateTo();
-
-    expect(page.getToggleFlightsButtonText()).toEqual('Toggle Flights');
+  it('should have a nav element', async () => {
+    await page.navigateTo();
+    expect(await page.getNavBar().isPresent()).toBeTruthy();
   });
 
-  it('should have a 5 flights displayed', () => {
-    page.navigateTo();
+  it('should have an app-buy-flights element', async () => {
+    await page.navigateTo();
+    expect(await page.getBuyFlightsElement().isPresent()).toBeTruthy();
+  });
+  it('should have a Toggle Flights button', async () => {
+    await page.navigateTo();
 
-    expect(page.getFlightTableRows()).toBe(6);
+    expect(await page.getToggleFlightsButtonText()).toEqual('Toggle Flights');
   });
 
-  it('should have a 0 flights displayed when flight toggle is clicked', () => {
-    page.navigateTo();
-    page.clickToggleFlights();
-    expect(page.getFlightTableRows()).toBe(0);
+  it('should have a 5 flights displayed', async () => {
+    await page.navigateTo();
+
+    expect(await page.getFlightTableRows()).toBe(5);
+  });
+
+  it('should have a 0 flights displayed when flight toggle is clicked', async () => {
+    await page.navigateTo();
+    await page.clickToggleFlights();
+    await browser.waitForAngular();
+    expect(await page.getFlightTableRows()).toBe(0);
   });
 
   afterEach(async () => {
--- Solutions/Ex4.1_Bonus_2/src/app/flights/flights.service.spec.ts
+++ Solutions/Ex4.1_Bonus_2/src/app/flights/flights.service.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { FlightsService } from '../flights/flights.service';
+import { TestBed } from '@angular/core/testing';
+
+import { FlightsService } from './flights.service';
 
 describe('FlightsService', () => {
   let service: FlightsService;
+
   beforeEach( async () => {
     await TestBed.configureTestingModule({
       providers: [FlightsService]
@@ -12,13 +14,5 @@ describe('FlightsService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
-  });
-
-  it('should return 5 flights from getFlights()', () => {
-    expect(service.getFlights().length).toBe(5);
-  });
-
-  it('should return 2 flights from getMyFlights()', () => {
-    expect(service.getMyFlights().length).toBe(2);
   });
 });
