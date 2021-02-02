102b72c135a4c73b7d0e5b0b93edaf51d2963dfb
Mike Way <mikeway@webwrights.co.uk>
2021-02-02 17:55:44 +0000

    course check e2e for start 2.1

---
+++ Solutions/Ex1.1/e2e/src/course-check.po.ts
@@ -0,0 +1,15 @@
+import { browser, element, by } from 'protractor';
+
+export class FlySharpCourseCheckPage {
+  async navigateTo(): Promise<unknown> {
+    return browser.get(browser.baseUrl);
+  }
+
+  async getParagraphText(): Promise<string> {
+    return element(by.css('app-root h1')).getText();
+  }
+
+  async getAppHomeH1(): Promise<string> {
+    return element(by.css('app-home h1')).getText();
+  }
+}
