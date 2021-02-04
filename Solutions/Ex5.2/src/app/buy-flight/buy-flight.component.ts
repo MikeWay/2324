4db293a629d8b6a5752a56f2822126ad2ff31a66
Mike Way <mikeway@webwrights.co.uk>
2021-02-04 13:39:12 +0000

    EX5.2

--- Solutions/Ex5.2/src/app/buy-flight/buy-flight.component.ts
+++ Solutions/Ex5.2/src/app/buy-flight/buy-flight.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {FlightsService} from '../flights/flights.service';
-import {Flight} from '../model/flight';
+import { FlightsService } from '../flights/flights.service';
+import { Flight } from '../model/flight';
 
 @Component({
   selector: 'app-buy-flight',
@@ -8,32 +8,45 @@ import {Flight} from '../model/flight';
   styleUrls: ['./buy-flight.component.css']
 })
 export class BuyFlightComponent implements OnInit {
-
-
   // tslint:disable-next-line: variable-name
-  _flights: (Flight)[] = new Array<Flight>();
+  _flights: Flight[] = new Array<Flight>();
   showBuyFlights = true;
-  selectedFlight: Flight | undefined;
+  // tslint:disable-next-line: variable-name
+  _selectedFlight: Flight | undefined;
+
   originFilter: string | null = null;
 
-  constructor( private flightsService: FlightsService ) { }
+  constructor(private flightsService: FlightsService) { }
 
   ngOnInit(): void {
     this._flights = this.flightsService.getFlights();
-  }
-
-  onFilterChange(filterValue: string): void {
-    this.originFilter = filterValue;
   }
 
   onClickBuyFlights(): void {
     this.showBuyFlights = !this.showBuyFlights;
   }
 
+  onFlightClick(flight: Flight): void {
+    this._selectedFlight = flight;
+  }
+
+  get selectedFlight(): Flight | undefined {
+    return this._selectedFlight;
+  }
+
+  set selectedFlight(flight: Flight | undefined) {
+    this._selectedFlight = flight;
+  }
+
+  onFilterChange(filterValue: string): void {
+    this.originFilter = filterValue;
+  }
+
+  /**
+   * Version of the flight getter that implements a simple filter
+   */
+
   get flights(): Flight[] {
-    /**
-     * Version of the flight getter that implements a simple filter
-     */
 
     if (this.originFilter) {
       return this._flights.filter((flight: Flight) => {
@@ -44,8 +57,6 @@ export class BuyFlightComponent implements OnInit {
     }
   }
 
-  onFlightClick(flight: Flight): void {
-    this.selectedFlight = flight;
-  }
 }
+
 
