fdf83ec51b8402954a92c9b6181061329f446792
Mike Way <mikeway@webwrights.co.uk>
2021-02-04 08:37:52 +0000

    Course check ex number change

--- Solutions/Ex3.1/e2e/src/course-check.e2e-spec.ts
+++ Solutions/Ex3.1/e2e/src/course-check.e2e-spec.ts
@@ -1,7 +1,7 @@
 import {FlySharpCourseCheckPage} from './course-check.po';
 import { browser, logging } from 'protractor';
 
-describe('Validate exercise 3.1_bonus start', () => {
+describe('Validate exercise 4.1 start', () => {
     let page: FlySharpCourseCheckPage;
 
     beforeEach(() => {
