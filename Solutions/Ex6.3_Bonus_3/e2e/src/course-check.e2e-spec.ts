2c3818780973f12d14b45ff9569698a2ddbe3d36
Mike Way <mikeway@webwrights.co.uk>
2021-02-05 17:38:36 +0000

    6.3_b_3

--- Solutions/Ex6.3_Bonus_3/e2e/src/course-check.e2e-spec.ts
+++ Solutions/Ex6.3_Bonus_3/e2e/src/course-check.e2e-spec.ts
@@ -1,4 +1,4 @@
-import {FlySharpCourseCheckPage} from './course-check.po';
+import { FlySharpCourseCheckPage } from './course-check.po';
 import { browser, logging } from 'protractor';
 
 describe('Validate exercise 7.1 start', () => {
@@ -9,112 +9,89 @@ describe('Validate exercise 7.1 start', () => {
   });
 
 
-  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
-    page.navigateToTab('home');
-    expect(page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
+  it('should display message saying Special Offer of the month 10% off all round-the-World flights', async () => {
+    await page.navigateTo();
+    expect(await page.getParagraphText()).toEqual('Special Offer of the month 10% off all round-the-World flights');
   });
-  it('should have an App-Home component', () => {
-    page.navigateTo();
-    expect(page.getAppHomeH1()).toEqual('Special Offer of the month 10% off all round-the-World flights');
-  });
-
-  it('should have a nav element', () => {
-    page.navigateTo();
-    expect(page.getNavBar().isPresent()).toBeTruthy();
+  it('should have an App-Home component', async () => {
+    await page.navigateTo();
+    expect(await page.getAppHomeH1()).toEqual('Special Offer of the month 10% off all round-the-World flights');
   });
 
-  it('should have an app-buy-flights element', () => {
-    page.navigateToTab('buy');
-    expect(page.getBuyFlightsElement().isPresent()).toBeTruthy();
-  });
-  it('should have a Toggle Flights button', () => {
-    page.navigateToTab('buy');
-
-    expect(page.getToggleFlightsButtonText()).toEqual('Toggle Flights');
+  it('should have a nav element', async () => {
+    await page.navigateTo();
+    expect(await page.getNavBar().isPresent()).toBeTruthy();
   });
 
-  it('should have a 5 flights displayed', () => {
-    page.navigateToTab('buy');
+  it('should have an app-buy-flights element', async () => {
+    await page.navigateToTab('buy');
+    expect(await page.getBuyFlightsElement().isPresent()).toBeTruthy();
+  });
+  it('should have a Toggle Flights button', async () => {
+    await page.navigateToTab('buy');
 
-    expect(page.getFlightTableRows()).toBe(6); // One for the header
+    expect(await page.getToggleFlightsButtonText()).toEqual('Toggle Flights');
   });
 
-  it('should have a 0 flights displayed when flight toggle is clicked', () => {
-    page.navigateToTab('buy');
-    page.clickToggleFlights();
-    expect(page.getFlightTableRows()).toBe(0);
-  });
-  it('should show 9 columns in the table', () => {
-    page.navigateToTab('buy');
-    expect(page.getNumTableCols()).toEqual(9);
+  it('should have a 5 flights displayed', async () => {
+    await page.navigateToTab('buy');
+
+    expect(await page.getFlightTableRows()).toBe(5);
   });
 
-  it('flight number for 5th flight should be FS2211', () => {
-    page.navigateToTab('buy');
-    expect(page.getTableCellData('5', '2')).toBe('FS2211');
+  it('should have a 0 flights displayed when flight toggle is clicked', async () => {
+    await page.navigateToTab('buy');
+    await page.clickToggleFlights();
+    await browser.waitForAngular();
+    expect(await page.getFlightTableRows()).toBe(0);
+  });
+  it('should show 9 columns in the table', async () => {
+    await page.navigateToTab('buy');
+    expect(await page.getNumTableCols()).toEqual(9);
   });
 
-  it('destination for 5th flight should be LHR', () => {
-    page.navigateToTab('buy');
-    expect(page.getTableCellData('5', '4')).toBe('LHR');
+  it('flight number for 6th flight should be FS2211', async () => {
+    await page.navigateToTab('buy');
+    expect(await page.getTableCellData('5', '2')).toBe('FS2211');
   });
 
-  it('should have an h1 element with Special Offer on the home tab', () => {
-    // There should not yet be a router so these should not change anything
-
-    page.navigateToTab('home');
-    expect(page.getAppHomeH1()).toContain('Special Offer');
-
+  it('destination for 5th flight should be LHR', async () => {
+    await page.navigateToTab('buy');
+    expect(await page.getTableCellData('5', '4')).toBe('LHR');
   });
 
-  it('should have an app-buy-flights element on the buy tab', () => {
-    // There should not yet be a router so these should not change anything
-    page.navigateToTab('buy');
-    expect(page.getBuyFlightsElement().isPresent()).toBeTruthy();
+  it('should have a app-payment element when a flight is selected', async () => {
+    await page.navigateToTab('buy');
+    await page.clickBuyFlight();
+    await browser.waitForAngular();
+    expect(page.getPaymentElement().isPresent()).toBeTruthy();
   });
 
-
-  it('should not have an app-buy-flights element on the home tab', () => {
-    // There should not yet be a router so these should not change anything
-    page.navigateToTab('home');
-    expect(page.getBuyFlightsElement().isPresent()).toBeFalsy();
-  });
-
-  it('should say my-flights works! on the My Flights tab', () => {
-    // There should not yet be a router so these should not change anything
-    page.navigateToTab('myflights');
-    expect(page.getMyFlightsParagraphText()).toBe('my-flights works!');
-
+  it('should have a app-flight-filter element', async () => {
+    await page.navigateToTab('buy');
+    expect(await page.getFlightFilterElement().isPresent()).toBeTruthy();
   });
 
 
-  it('should say account works! on the Account tab', () => {
-    // There should not yet be a router so these should not change anything
-    page.navigateToTab('account');
-    expect(page.getAccountParagraphText()).toBe('account works!');
-
+  it('should have a router-outlet', async () => {
+    await page.navigateToTab('buy');
+    expect(await page.getRouterOutlet().isPresent()).toBeTruthy();
   });
 
-
-  it('The price column should contain USD', () => {
-    page.navigateToTab('buy');
-    page.getTableCellData('1', '8').then(value => console.log('===============>' + value));
-    expect(page.getTableCellData('2', '9')).toContain('USD');
+  it('The price column should contain USD', async () => {
+    await page.navigateToTab('buy');
+    expect(await page.getTableCellData('2', '9')).toContain('USD');
   });
 
-  it('should have a Payment Component when the buy button is pressed', () => {
-    page.navigateToTab('buy');
-    page.clickBuyFlightButton();
-    expect(page.getPaymentComponentElement().isPresent()).toBeTruthy();
-  });
-/* Tests from here are checks that we have not accidentally got the solution from subsequent exercises */
+  /* Tests from here are checks that we have not accidentally got the solution from subsequent exercises */
 
-  it('should have NOT have a Payment Component FORM when the buy button is pressed', () => {
-    page.navigateToTab('buy');
-    page.clickBuyFlightButton();
-    expect(page.getPaymentForm().isPresent()).toBeFalsy();
-  });
 
+  it('should NOT have a Payment Component FORM when the buy button is pressed',async  () => {
+    await page.navigateToTab('buy');
+    await page.clickBuyFlight();
+    await browser.waitForAngular();
+    expect(await page.getPaymentForm().isPresent()).toBeFalsy();
+  });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
--- Solutions/Ex6.3_Bonus_3/src/app/flights/flights.service.spec.ts
+++ Solutions/Ex6.3_Bonus_3/src/app/flights/flights.service.spec.ts
@@ -1,22 +1,25 @@
-import { TestBed, inject } from '@angular/core/testing';
-import { FlightsService } from '../flights/flights.service';
+import { TestBed } from '@angular/core/testing';
+
+import { FlightsService } from './flights.service';
 
 describe('FlightsService', () => {
+  let service: FlightsService;
   beforeEach( async () => {
     await TestBed.configureTestingModule({
       providers: [FlightsService]
     });
+    service = TestBed.inject(FlightsService);
   });
 
-  it('should be created', inject([FlightsService], (service: FlightsService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should return 5 flights from getFlights()', inject([FlightsService], (service: FlightsService) => {
+  it('should return 5 flights from getFlights()', () => {
     expect(service.getFlights().length).toBe(5);
   });
 
-  it('should return 2 flights from getMyFlights()', inject([FlightsService], (service: FlightsService) => {
+  it('should return 2 flights from getMyFlights()', () => {
     expect(service.getMyFlights().length).toBe(2);
   });
 });
