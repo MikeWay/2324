63e1ca3a3764289c7e38b83329d1f089c1b9f719
mikeway <mikeway@webwrights.co.uk>
2021-08-02 07:11:14 -0700

    Removed old app.e2e files from solutions

--- NodeScripts/hack.cmd
+++ NodeScripts/hack.cmd
@@ -1,6 +1,8 @@
-FOR %%D IN ( Ex6.2,Ex6.2_Bonus_1,Ex6.2_Bonus_2,Ex6.2_Bonus_3,Ex7.1,Ex7.1_Bonus_1,Ex7.2,Ex7.2_Bonus_1,Ex8.1,Ex8.1_Bonus_1,Ex8.1_Bonus_2,Ex9.1,Ex9.1_Bonus_1 ) DO (
+FOR %%D IN ( Ex2.1,Ex2.1_Bonus,Ex2.2,Ex2.2_Bonus,Ex2.2_Bonus_2,Ex3.1,Ex4.1,Ex4.1_Bonus,Ex4.1_Bonus_1,Ex4.1_Bonus_2,Ex4.2,Ex4.2_Bonus_1,Ex5.1,Ex5.1_Bonus_1,Ex5.2,Ex5.2_Bonus_1,Ex5.2_Bonus_2,Ex5.2_Bonus_3,Ex6.1,Ex6.1_Bonus_1,Ex6.1_Bonus_2,Ex6.2,Ex6.2_Bonus_1,Ex6.2_Bonus_2,Ex6.2_Bonus_3,Ex7.1,Ex7.1_Bonus_1,Ex7.2,Ex7.2_Bonus_1,Ex8.1,Ex8.1_Bonus_1,Ex8.1_Bonus_2,Ex9.1,Ex9.1_Bonus_1 ) DO (
 
- copy  C:\course2324\Exercises\FlySharp\src\app\app.component.spec.ts C:\course2324\Solutions\%%D\src\app\.
+ rem copy  C:\course2324\Exercises\FlySharp\src\app\app.component.spec.ts C:\course2324\Solutions\%%D\src\app\.
+ del C:\course2324\Solutions\%%D\e2e\src\app.e2e-spec.ts
+ del C:\course2324\Solutions\%%D\e2e\src\app.po.ts
 )
 
 
