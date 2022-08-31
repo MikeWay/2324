e23bd5c375a6ac0e213daa00d11c1f8fa4ef4893
Mike Way <mikeway@webwrights.co.uk>
2022-08-31 16:07:51 +0100

    Corrected more ActivatedRoute accessors

--- Solutions/Ex6.3_Bonus_2/src/app/buy-flight/buy-flight.component.ts
+++ Solutions/Ex6.3_Bonus_2/src/app/buy-flight/buy-flight.component.ts
@@ -23,7 +23,7 @@ export class BuyFlightComponent implements OnInit {
   constructor(private flightsService: FlightsService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params => this.originFilter = params.origin);
+    this.activatedRoute.params.subscribe(params => this.originFilter = params['origin']);
     this._flights = this.flightsService.getFlights();
   }
 
