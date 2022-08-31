c595aafd84c5c664321a96c4a82b7afd247ebb03
Mike Way <mikeway@webwrights.co.uk>
2022-08-31 14:26:01 +0100

    Updated Payment.Component changed ?. to .

--- NodeScripts/hack.cmd
+++ NodeScripts/hack.cmd
@@ -1,10 +1,12 @@
-FOR %%D IN ( Ex5.1,Ex5.1_Bonus_1,Ex5.2,Ex5.2_Bonus_1,Ex5.2_Bonus_2,Ex5.2_Bonus_3,Ex6.1,Ex6.1_Bonus_1,Ex6.1_Bonus_2,Ex6.2,Ex6.2_Bonus_1,Ex6.2_Bonus_2,Ex6.2_Bonus_3,Ex7.1,Ex7.1_Bonus_1,Ex7.2,Ex7.2_Bonus_1,Ex8.1,Ex8.1_Bonus_1,Ex8.1_Bonus_2,Ex9.1,Ex9.1_Bonus_1 ) DO (
+FOR %%D IN ( Ex5.2,Ex5.2_Bonus_1,Ex5.2_Bonus_2,Ex5.2_Bonus_3,Ex6.1,Ex6.1_Bonus_1,Ex6.1_Bonus_2,Ex6.2,Ex6.2_Bonus_1,Ex6.2_Bonus_2,Ex6.2_Bonus_3,Ex7.1,Ex7.1_Bonus_1,Ex7.2,Ex7.2_Bonus_1,Ex8.1,Ex8.1_Bonus_1,Ex8.1_Bonus_2,Ex9.1,Ex9.1_Bonus_1 ) DO (
 
- del C:\course2324\Solutions\%%D\cypress\integration\spec.ts
- rmdir C:\course2324\Solutions\%%D\cypress\integration
- mkdir C:\course2324\Solutions\%%D\cypress\e2e\
- copy  C:\course2324\Exercises\FlySharp\cypress\e2e\spec.cy.ts C:\course2324\Solutions\%%D\cypress\e2e\.
+ rem del C:\course2324\Solutions\%%D\cypress\integration\spec.ts
+ rem rmdir C:\course2324\Solutions\%%D\cypress\integration
+ rem mkdir C:\course2324\Solutions\%%D\cypress\e2e\
+ rem copy  C:\course2324\Exercises\FlySharp\cypress\e2e\spec.cy.ts C:\course2324\Solutions\%%D\cypress\e2e\.
  rem del C:\course2324\Solutions\%%D\e2e\src\app.e2e-spec.ts
+
+ COPY C:\course2324\Exercises\FlySharp\src\app\payment\payment.component.html C:\course2324\Solutions\%%D\src\app\payment\.
 
 )
 
