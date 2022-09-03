52ad6671cec72d8cbbb8663b20f06e60d8f3659c
Mike Way <mikeway@webwrights.co.uk>
2022-09-03 11:30:47 +0100

    Updated Ex7.2_b_1

--- Solutions/Ex7.2_Bonus_1/src/app/app.module.ts
+++ Solutions/Ex7.2_Bonus_1/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule    
   ],
   providers: [FlightsService],
   bootstrap: [AppComponent]
