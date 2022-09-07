6e00b5ca86d810595fb5f2a9de22c88e0192a5bd
mikeway <mikeway@webwrights.co.uk>
2022-09-07 09:57:43 -0400

    Reset DN41/42 start point

--- DoNows/DoNow41/src/app/app.component.spec.ts
+++ DoNows/DoNow41/src/app/app.component.spec.ts
@@ -15,7 +15,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent, MockWeatherComponent
+        AppComponent
       ],
     }).compileComponents();
   });
