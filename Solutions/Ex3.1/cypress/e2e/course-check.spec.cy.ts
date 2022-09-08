31612c589e2ceb7780dbc9589a247d24f93a743a
mikeway <mikeway@webwrights.co.uk>
2022-09-08 09:15:43 -0400

    Course check for start ex4.1

---
+++ Solutions/Ex3.1/cypress/e2e/course-check.spec.cy.ts
@@ -0,0 +1,127 @@
+describe('Course Check - Ex4.1 Start', () => {
+  it('should display message saying Special Offer of the month 10% off all round-the-World flights', () => {
+    cy.visit('/');
+    cy.contains('Special Offer of the month 10% off all round-the-World flights')
+  })
+
+  it('should have an App-Home component', () => {
+    cy.visit('/');
+    cy.get('app-home h1').should('have.text','Special Offer of the month 10% off all round-the-World flights');
+  });  
+
+
+  it('should have a nav element', () => {
+    cy.visit('/');
+    cy.get('app-root nav').should('be.visible');
+  });  
+
+  it('should have an app-buy-flights element', () => {
+    cy.visit('/');
+    cy.get('app-root app-buy-flight').should('be.visible');
+  }); 
+
+  // End positive tests for Ex3.1
+  // Following tests are to verify that code has not been completed!
+
+  it('should have a 5 flights displayed', () => {
+    cy.visit('/');
+    //cy.get('app-root app-buy-flight a').click();
+    cy.get('app-buy-flight table tbody tr').should('have.length', 5);
+  }); 
+
+
+})
+
+/*
+import { FlySharpCourseCheckPage } from './course-check.po';
+import { browser, logging } from 'protractor';
+
+/** ! IMPORTANT! These tests will run very slowly or fail due to the clock updates causing Protractor to fail to detect that Angular is stable
+ * The only solution I have found so far is to add a flag to turn off the clock
+*/
+/*
+
+
+
+
+  
+  
+
+  
+    it('should show 9 columns in the table', async () => {
+      await page.navigateToTab('buy');
+      expect(await page.getNumTableCols()).toEqual(9);
+    });
+  
+    it('flight number for 6th flight should be 114', async () => {
+      await page.navigateToTab('buy');
+      expect(await page.getTableCellData('5', '2')).toBe('114');
+    });
+  
+    it('destination for 5th flight should be LHR', async () => {
+      await page.navigateToTab('buy');
+      expect(await page.getTableCellData('5', '4')).toBe('JFK');
+    });
+  
+    it('should have a app-payment element when a flight is selected', async () => {
+      await page.navigateToTab('buy');
+      await page.clickBuyFlight();
+      await browser.waitForAngular();
+      expect(page.getPaymentElement().isPresent()).toBeTruthy();
+    });
+  
+    it('should have a app-flight-filter element', async () => {
+      await page.navigateToTab('buy');
+      expect(await page.getFlightFilterElement().isPresent()).toBeTruthy();
+    });
+  
+  
+    it('should have a router-outlet', async () => {
+      await page.navigateToTab('buy');
+      expect(await page.getRouterOutlet().isPresent()).toBeTruthy();
+    });
+  
+    it('The price column should contain USD', async () => {
+      await page.navigateToTab('buy');
+      expect(await page.getTableCellData('2', '9')).toContain('USD');
+    });
+  
+    it('should have a Payment Component FORM when the buy button is pressed', async () => {
+      await page.navigateToTab('buy');
+      await page.clickBuyFlight();
+      await browser.waitForAngular();
+      expect(await page.getPaymentForm().isPresent()).toBeTruthy();
+    });
+  
+  
+    it('should have an input with a formcontrolname attribute when the buy button is pressed', async () => {
+      await page.navigateToTab('buy');
+      await page.clickBuyFlight();
+      await browser.waitForAngular();
+      expect(await page.getFormControlNameAttributeFromPaymentForm()).toBeTruthy();
+    });
+  
+  
+    it('should have a app-flight-status element', async () => {
+      await page.navigateToTab('home');
+      expect(await page.getFlightStatusElement().isPresent()).toBeTruthy();
+    });
+  
+    /* Tests from here are checks that we have not accidentally got the solution from subsequent exercises */
+  /*
+    it('should not have elements containing AppTime', () => {
+      expect(page.getElementContainingAppTime().isPresent()).toBeFalsy();
+    });
+  
+  
+    afterEach(async () => {
+      // Assert that there are no errors emitted from the browser
+      const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+      expect(logs).not.toContain(jasmine.objectContaining({
+        level: logging.Level.SEVERE,
+      } as logging.Entry));
+    });
+  });
+  */
+
+
