572c075599314dace80cd869a57da9e8a7830ec0
Mike Way <mikeway@webwrights.co.uk>
2023-01-12 18:10:15 +0000

    Updated Ex2.1_Bonus Solution

--- Solutions/Ex2.1_Bonus/src/app/app.module.ts
+++ Solutions/Ex2.1_Bonus/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { HomeComponent } from './home/home.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
