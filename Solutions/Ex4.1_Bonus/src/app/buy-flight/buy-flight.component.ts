35c74462f90497502e0c38e5963e81cdbaeef1f6
Mike Way <mikeway@webwrights.co.uk>
2023-01-14 12:55:10 +0000

    Updated solution to Ex4.1_Bonus

--- Solutions/Ex4.1_Bonus/src/app/buy-flight/buy-flight.component.ts
+++ Solutions/Ex4.1_Bonus/src/app/buy-flight/buy-flight.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FlightsService } from '../flights/flights.service';
+import { ApplicationStateService } from '../application-state/application-state.service';
 import { Flight } from '../model/flight';
 
 @Component({
   selector: 'app-buy-flight',
   templateUrl: './buy-flight.component.html',
-  styleUrls: ['./buy-flight.component.css']
+  styleUrls: ['./buy-flight.component.scss']
 })
 export class BuyFlightComponent implements OnInit {
-
-  flights: Flight[] = new Array<Flight>();
+  flights! : Flight[];
+  selectedFlight: Flight | undefined;
   showBuyFlights = true;
 
-  constructor(private flightsService: FlightsService) { }
+  constructor(private stateService: ApplicationStateService){}
+
+  onFlightClick(flight : Flight): void {
+    this.selectedFlight = flight;
+
+}  
+  
+  onClickBuyFlights(){
+    this.showBuyFlights = !this.showBuyFlights;
+  }  
 
   ngOnInit(): void {
-    this.flights = this.flightsService.getFlights();
+    this.flights = this.stateService.getFlights();    
   }
 
-  onClickBuyFlights(): void {
-    this.showBuyFlights = !this.showBuyFlights;
-  }
 }
-
 
