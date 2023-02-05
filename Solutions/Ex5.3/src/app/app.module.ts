23966b01864bd026b49d129f0e9af6273812796e
Mike Way <mikeway@webwrights.co.uk>
2023-02-05 17:31:00 +0000

    Ex5.3 Solution

---
+++ Solutions/Ex5.3/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BuyFlightComponent } from './buy-flight/buy-flight.component';
+import { PaymentComponent } from "./payment/payment.component";
+import { FlightFilterComponent } from './flight-filter/flight-filter.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent        
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        PaymentComponent,
+        FlightFilterComponent
+    ]
+})
+export class AppModule { }
