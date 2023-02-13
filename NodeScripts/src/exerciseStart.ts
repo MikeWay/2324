fd8fbf1c600986301ede4958c1575ebfd85127be
Mike Way <mikeway@webwrights.co.uk>
2023-02-13 14:08:59 +0000

    Updates start script for ex7.1

--- NodeScripts/src/exerciseStart.ts
+++ NodeScripts/src/exerciseStart.ts
@@ -19,7 +19,7 @@ const EX_MAPPINGS = {
 	"Ex6.1": "Ex5.3_Bonus_1",
 	// "Ex6.2": "Ex6.1_Bonus_2",
 	// "Ex6.3": "Ex6.2",
-    "Ex7.1": "Ex6.3_Bonus_3",
+    "Ex7.1": "Ex6.2_Bonus_1",
     "Ex7.2": "Ex7.1_Bonus_1",
     "Ex8.1": "Ex7.2_Bonus_1",
 	"Ex8.2": "Ex8.1_Bonus_2",
