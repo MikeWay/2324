d2671c497177cc403fc335ce69d6b1cf84ef8a24
Mike Way <mikeway@webwrights.co.uk>
2023-02-15 14:32:01 +0000

    Deleted old 7.1 sol

--- Solutions/Ex7.1_Bonus_1/src/app/model/payment.ts
+++
@@ -1,9 +0,0 @@
-export class Payment {
-  constructor(
-    public name: string = '',
-    public address: string = '',
-    public email: string = '',
-    public cardNum: string = '',
-    public cardType: string = '',
-    public expDate: string = '') { }
-}
