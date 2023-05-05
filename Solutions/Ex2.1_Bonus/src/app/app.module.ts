c16b7e089a6b87432d07c5a95d8c57fbe1fe3238
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2023-05-05 10:23:07 +0100

    Updated ex2.1_Bonus

--- Solutions/Ex2.1_Bonus/src/app/app.module.ts
+++ Solutions/Ex2.1_Bonus/src/app/app.module.ts
@@ -7,8 +7,7 @@ import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent
+    AppComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
--- Solutions/Ex2.1_Bonus/src/app/home/home.component.ts
+++ Solutions/Ex2.1_Bonus/src/app/home/home.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<h1>Special Offer of the month {{specialOffer}}</h1>`,
+  template: `<h1>Special Offer of the month {{specialOffer}}</h1>`
 })
 export class HomeComponent {
-  specialOffer = '10% off all round-the-World flights';
+  specialOffer = `10% off all round-the-World flights`;
 }
