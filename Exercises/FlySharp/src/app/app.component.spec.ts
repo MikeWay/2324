f4e9a348ec450566f688831bb34f3ff9c973a366
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-06-24 16:35:36 +0100

    Test / fix to end of Ex1.1

--- Exercises/FlySharp/src/app/app.component.spec.ts
+++ Exercises/FlySharp/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'FlySharp' title`, () => {
+  it(`should have the 'Fly Sharp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('FlySharp');
+    expect(app.title).toEqual('Fly Sharp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FlySharp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Fly Sharp');
   });
 });
