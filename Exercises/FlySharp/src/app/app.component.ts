89cb40f9a5877d398cbaf1945d50b61d20c056f7
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-06-26 17:04:02 +0100

    Completed Ex2.2 and 1st bonus

--- Exercises/FlySharp/src/app/app.component.ts
+++ Exercises/FlySharp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BuyFlightComponent } from './buy-flight/buy-flight.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, BuyFlightComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Solutions/Ex2.2/src/app/app.component.ts
+++ Solutions/Ex2.2/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BuyFlightComponent } from './buy-flight/buy-flight.component';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent, BuyFlightComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Fly Sharp';
