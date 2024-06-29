9e4073bf9917574807ea0ba3d92ff33cc437d88c
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-06-29 17:49:59 +0100

    Reworked Ex4.2 (for A18)

--- Exercises/FlySharp/src/app/buy-flight/buy-flight.component.ts
+++ Exercises/FlySharp/src/app/buy-flight/buy-flight.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { ApplicationStateService } from '../application-state/application-state.service';
 import { Flight } from '../model/flight';
 import { PaymentComponent } from '../payment/payment.component';
+import { FlightFilterComponent } from '../flight-filter/flight-filter.component';
 
 @Component({
   selector: 'app-buy-flight',
   standalone: true,
-  imports: [PaymentComponent],
+  imports: [PaymentComponent, FlightFilterComponent],
   templateUrl: './buy-flight.component.html',
   styleUrl: './buy-flight.component.scss'
 })
 export class BuyFlightComponent implements OnInit {
-  flights!: Flight[];
+  _flights!: Flight[];
   showBuyFlights = true;
   selectedFlight: Flight | undefined;
+  originFilter = '';
 
   constructor(private stateService: ApplicationStateService)
   {}
@@ -26,8 +28,22 @@ export class BuyFlightComponent implements OnInit {
     this.showBuyFlights = !this.showBuyFlights;
   }
 
+  onOriginFilterChange(filterValue: string): void {
+    this.originFilter = filterValue;
+  }
+
+  get flights(): Flight[] {
+    return this._flights.filter(flight => this.orginDestinationFilter(flight)? flight : null);
+  }
+
+  private orginDestinationFilter(flight: Flight): boolean {
+    if (this.originFilter === '') return true;
+    if(flight.origin.startsWith(this.originFilter)) return true;
+      return false;
+  }
+
   ngOnInit(): void {
-    this.flights = this.stateService.getFlights();  
+    this._flights = this.stateService.getFlights();  
   }  
 }
 
