543b3f012e58cc7dca5990081a84a4851c305fd9
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-07-04 14:40:40 +0100

    Yet another attempt to get the start for DN61 right!

--- DoNows/projects/do-now61/src/app/current-weather/current-weather.component.spec.ts
+++ DoNows/projects/do-now61/src/app/current-weather/current-weather.component.spec.ts
@@ -14,19 +14,20 @@ describe('CurrentWeatherComponent', () => {
   // TODO 1 Uncomment the next line to create a Spy object  for the WeatherService
   //mockWeatherService = jasmine.createSpyObj('WeatherService',['getForecast']);    
 
-  // TODO 5 - Do this at the end! Remove the comments around the next 3 lines of code to enable the functionality
+  // TODO 6 - Do this at the end! Remove the comments around the next 3 lines of code to enable the functionality
   // for the getForecast()
   // mockWeatherService.getForecast.and.callFake((city: string) => {
   //   return `Some weather ${city}`;
-  // });The weather is lovely in Toronto (no, really)
+  // });
 
   await TestBed.configureTestingModule({
       imports: [ CurrentWeatherComponent ],
     }).overrideComponent(CurrentWeatherComponent, { 
-      remove: {providers: [WeatherService]}, // This code removes the real WeatherService from the dependency injector
+  // TODO 2 - Uncomment the next line to remove the real WeatherService from the dependency injector      
+      // remove: {providers: [WeatherService]}, 
       add:{
-  // TODO 2 - Uncomment the next line to provide the fake weather service          
-        providers: [{provide: WeatherService, useValue: mockWeatherService}]
+  // TODO 3 - Uncomment the next line to provide the fake weather service          
+        // providers: [{provide: WeatherService, useValue: mockWeatherService}]
       }
     })
     .compileComponents();
@@ -42,12 +43,12 @@ describe('CurrentWeatherComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  xit('should call getForecast on the Weather Service', () => {
+  it('should call getForecast on the Weather Service', () => {
     const weather = component.forecast;
     expect(mockWeatherService.getForecast).toHaveBeenCalled();
   });  
 
-  // TODO 3 - Examine the test below and then change xit to it in the line below to enable the next test
+  // TODO 4 - Examine the test below and then change xit to it in the line below to enable the next test
   xit('should call getForecast passing "Toronto" as the argument', () => {
     component.city = 'Toronto';
     const weather = component.forecast;
@@ -55,7 +56,7 @@ describe('CurrentWeatherComponent', () => {
     expect(mockWeatherService.getForecast).toHaveBeenCalledWith('Toronto');
   });    
 
-  // TODO 4 - Examine the test below and then change xit to it in the line below to enable the next test
+  // TODO 5 - Examine the test below and then change xit to it in the line below to enable the next test
   xit('should return the city as part of the weather string', () => {
     component.city = 'Toronto';
     expect(component.forecast).toContain('Toronto');
