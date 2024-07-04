d7353ef8355b5ac49cce57dcbd54f5241fdcb43d
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-07-04 10:43:19 +0100

    New start for DN61 - A19

---
+++ DoNows/projects/do-now61/src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../weather/weather.service';
+
+@Component({
+  selector: 'app-current-weather',
+  standalone: true,
+  providers: [WeatherService],
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent {
+
+  city= 'Manchester';
+
+  constructor(private weatherService : WeatherService) { }
+
+  get forecast(): string {
+    return this.weatherService.getForecast(this.city);
+  }
+
+}
