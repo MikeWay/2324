e9e5dd616f10de7968f68bf319ebf2ae1a80c1a6
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-07-08 15:47:05 +0100

    Updated DN72 Sol

---
+++ DoNows/projects/do-now72-sol/src/app/preferences-form/preferences-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, ViewChild } from '@angular/core';
+import { Preferences } from '../model/preferences';
+import { FormsModule, NgForm } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-preferences-form',
+  standalone: true,
+  imports: [FormsModule, JsonPipe],
+  templateUrl: './preferences-form.component.html',
+  styleUrl: './preferences-form.component.scss'
+})
+export class PreferencesFormComponent {
+  temperatureUnits = ['F', 'C', 'K'];
+  speedUnits = ['Knots', 'MPH', 'KPH' , 'M/S'];
+  
+  model = new Preferences(  "Long John Silver", "Treasure Island", "F", "Knots", 10 );
+
+  ngOnInit() {
+  }
+
+  get jsonData() { return JSON.stringify(this.model); }
+
+
+}
+
