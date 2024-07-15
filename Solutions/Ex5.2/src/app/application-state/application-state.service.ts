89f6d59933da3e8d1fb6f806f925b474735bea08
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-07-15 11:57:58 +0100

    Updated Ex5.2 Solution with the load flights stuff

--- Solutions/Ex5.2/src/app/application-state/application-state.service.ts
+++ Solutions/Ex5.2/src/app/application-state/application-state.service.ts
@@ -7,7 +7,7 @@ import { Flight } from '../model/flight';
 })
 export class ApplicationStateService {
 
-  _flights = FLIGHTS;
+  _flights = new Array<Flight>();
 
   public getFlights(): Flight[] {
     return this._flights;
@@ -16,4 +16,20 @@ export class ApplicationStateService {
   public getMyFlights(): Flight[] {
     return MYFLIGHTS;
   }  
+
+	
+  public loadFlights(start: number, count: number, origin?: string, destination?: string){
+
+    this._flights = FLIGHTS; // Simulate load from Web service
+    if (origin) {
+      this._flights = this._flights.filter((flight: Flight) => {
+          return flight.origin.startsWith(origin as string);
+      });
+    }
+    if (destination) {
+      this._flights = this._flights.filter((flight: Flight) => {
+          return flight.destination.startsWith(destination as string);
+      });
+    }    
+  }
 }
