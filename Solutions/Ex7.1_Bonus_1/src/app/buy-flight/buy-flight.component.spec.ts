3281ff21c604a95132422f8aba47028ba716b4f4
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-07-15 17:07:05 +0100

    Reworked Ex7.1_b_1

--- Solutions/Ex7.1_Bonus_1/src/app/buy-flight/buy-flight.component.spec.ts
+++ Solutions/Ex7.1_Bonus_1/src/app/buy-flight/buy-flight.component.spec.ts
@@ -1,72 +1,38 @@
-import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BuyFlightComponent } from './buy-flight.component';
+import { provideRouter } from '@angular/router';
+import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
-import { from } from 'rxjs';
 import { ApplicationStateService } from '../application-state/application-state.service';
-import { Flight } from '../model/flight';
 import { FLIGHTS, MYFLIGHTS } from '../model/mock-flights';
 
-import { BuyFlightComponent } from './buy-flight.component';
-
-class MockApplicationStateService {
-
-  displayCurrency = { code: 'GBP', symbol: '£', rate: 1.0 };
-
-  flights = FLIGHTS;
-
-  public getFlights(): Flight[] {
-    return FLIGHTS;
-  }
-
-  public getMyFlights(): Flight[] {
-    return MYFLIGHTS;
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
-  public loadFlights(start: number, count: number, origin?: string, destination?: string) {
-  }
+let stateServiceStub: Partial<ApplicationStateService> = {
+  getFlights: () => FLIGHTS,
+  displayCurrency: { code: 'USD', symbol: '$', rate: 0.9 }
 }
 
-let spyAppState: any;
+let spyAppState = null;
 
 describe('BuyFlightComponent', () => {
   let component: BuyFlightComponent;
   let fixture: ComponentFixture<BuyFlightComponent>;
   let el: DebugElement;
 
-
   beforeEach(async () => {
-
-    spyAppState = jasmine.createSpyObj<ApplicationStateService>('MyApplicationStateService', [ 'getFlights', 'getMyFlights','loadFlights'],
-    {
-      displayCurrency: { code: 'GBP', symbol: '£', rate: 1.0 }
-    });
+    spyAppState = jasmine.createSpyObj<ApplicationStateService>('MockApplicationStateService', ['getFlights', 'getMyFlights', 'loadFlights'],
+      {
+        displayCurrency: { code: 'GBP', symbol: '£', rate: 1.0 }
+      });
 
     spyAppState.getFlights.and.returnValue(FLIGHTS);
     spyAppState.getMyFlights.and.returnValue(MYFLIGHTS);
 
-    //spyAppState = new MockApplicationStateService();
     await TestBed.configureTestingModule({
-      imports: [ BuyFlightComponent ],
-      providers:[
-        /* BuyFlightComponent takes an ActivatedRoute as a constructor argument. In our code we are accessing
-         the params of property ActivatedRoute. The useValue code below creates a very simple Observable as the 
-         value of params. It is adequate to meet the needs of the test so far
-         */   
-        {
-          provide: ApplicationStateService,
-          useValue: spyAppState
-        },     
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            params: from([{ id: 1 }]),
-          }
-        }
-      ]
-    })
-    .compileComponents();
+      imports: [BuyFlightComponent],
+      providers: [provideRouter([])]
+    }).overrideComponent(BuyFlightComponent,
+      { set: { providers: [{ provide: ApplicationStateService, useValue: spyAppState }] } }
+    )
 
     fixture = TestBed.createComponent(BuyFlightComponent);
     component = fixture.componentInstance;
@@ -79,37 +45,36 @@ describe('BuyFlightComponent', () => {
 
   it('should default showBuyFlights to true', () => {
     expect(component.showBuyFlights).toBeTruthy();
-  });  
+  });
 
   it('should set showBuyFlights to false when onClickBuyFlights() is called', () => {
     component.onClickBuyFlights();
     expect(component.showBuyFlights).toBeFalsy();
-  });  
+  });
 
-  it('should set showBuyFlights to true when onClickBuyFlights() is called', () => {
+  it('should set showBuyFlights to true when onClickBuyFlights() is called twice', () => {
     component.onClickBuyFlights();
     component.onClickBuyFlights();
     expect(component.showBuyFlights).toBeTruthy();
-  });  
+  });
 
   it('should set showBuyFlights to false when the link is clicked', () => {
     el = fixture.debugElement.query(By.css('a'));
     el.triggerEventHandler('click', null);
     expect(component.showBuyFlights).toBeFalsy();
-  });  
+  });
 
   it('should hide the flights table  when the link is clicked', () => {
-    fixture.detectChanges();
     let tableEle = fixture.debugElement.query(By.css('table'));
-    expect(tableEle).toBeTruthy();
+    expect(component.showBuyFlights).toBeTruthy();
     el = fixture.debugElement.query(By.css('a'));
     el.triggerEventHandler('click', null);
     fixture.detectChanges();
     tableEle = fixture.debugElement.query(By.css('table'));
     expect(tableEle).toBeFalsy();
-  });  
+  });
 
-  it('should call getFlights twice', () => {
-    expect((spyAppState).getFlights.calls.count()).toBe(2);
-  });  
+  it('should have called getFlights() once', () => {
+    expect(spyAppState!.getFlights.calls.count()).toBe(1);
+  });
 });
--- Solutions/Ex7.1_Bonus_1/src/app/payment/payment.component.ts
+++ Solutions/Ex7.1_Bonus_1/src/app/payment/payment.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Flight } from '../model/flight';
 import { FormsModule } from '@angular/forms';
 import { Payment } from '../model/payment';
@@ -7,36 +6,35 @@ import { Payment } from '../model/payment';
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [FormsModule],
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.scss']
+  styleUrl: './payment.component.scss'
 })
 export class PaymentComponent {
-  _selectedFlight: Flight | undefined;
+  private _selectedFlight: Flight | undefined;
   model: Payment = new Payment();
-  @Output() paymentConfirmed: EventEmitter<FlightPaymentEvent> = new EventEmitter();
-  
+  @Output()
+  paymentConfirmed: EventEmitter<FlightPaymentEvent> = new EventEmitter<FlightPaymentEvent>();
+
   @Input()
   get selectedFlight(): Flight | undefined {
     return this._selectedFlight;
   }
-
-  set selectedFlight(flight: Flight | undefined){
+  set selectedFlight(flight: Flight | undefined) {
     this._selectedFlight = flight;
   }
-
   get jsonModel(): string {
     return JSON.stringify(this.model);
-  }   
+  }
 
-  onSubmit(): void {
-    if(this.selectedFlight){
-      const payment = new FlightPaymentEvent(this.selectedFlight, this.model);
-      this.paymentConfirmed.emit(payment);
+  onSubmit() {
+    if(this._selectedFlight){
+      this.paymentConfirmed.emit(new FlightPaymentEvent(this._selectedFlight, this.model));
     }
-  }  
-}
+  }
 
+}
+	
 export class FlightPaymentEvent {
   constructor( public flight: Flight, public payment: Payment){}
 }
