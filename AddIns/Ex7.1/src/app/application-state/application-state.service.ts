274eace23330efa3404d289fac288741b8bc0f23
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-07-22 17:09:55 +0100

    Updated AppStSvc for Ex7.1 AddIns

--- AddIns/Ex7.1/src/app/application-state/application-state.service.ts
+++ AddIns/Ex7.1/src/app/application-state/application-state.service.ts
@@ -1,60 +1,41 @@
 import { Injectable } from '@angular/core';
-import { Currency } from '../model/currency';
-import { Flight } from '../model/flight';
 import { FLIGHTS, MYFLIGHTS } from '../model/mock-flights';
+import { Flight } from '../model/flight';
+import { Currency } from '../model/currency';
 
-@Injectable(
-{
+@Injectable({
   providedIn: 'root'
 })
-
 export class ApplicationStateService {
 
+  _flights = new Array<Flight>();
   _myFlights: Flight[] = new Array<Flight>();
-
   currencies: Currency[] = [
     { code: 'GBP', symbol: '£', rate: 1.0 },
     { code: 'USD', symbol: '$', rate: 0.9 },
-    { code: 'EUR', symbol: '€', rate: 0.92 },
+    { code: 'EUR', symbol: '€', rate: 0.92 },  
     { code: 'SEK', symbol: 'kr ', rate: 12.0 }
   ];
+  
+  displayCurrency: Currency = this.currencies[1];  
 
-  displayCurrency: Currency = this.currencies[1];
-
-  _flights = FLIGHTS;
-
-  constructor(){
-    this.myFlights.push(...MYFLIGHTS);
+  constructor() {
+    this.loadFlights();
   }
-
   public get flights(): Flight[] {
     return this._flights;
   }
 
-  public getFlights(): Flight[]{
-    return this._flights;
-  }
-
   public get myFlights(): Flight[] {
-    return this._myFlights;
-  }
+    return MYFLIGHTS;
+  }  
 
   addMyFlight(flight: Flight): number {
     this.myFlights.push(flight);
     return this.myFlights.length;
   }   
 
-  public loadFlights(start: number, count: number, origin?: string, destination?: string) {
-    this._flights = FLIGHTS;
-    if (origin) {
-      this._flights = FLIGHTS.filter((flight: Flight) => {
-        return flight.origin.startsWith(origin as string);
-      });
-    }
-    if (destination) {
-      this._flights = this._flights.filter((flight: Flight) => {
-        return flight.destination.startsWith(destination as string);
-      });
-    }
-  }
+  private loadFlights(){
+    this._flights = FLIGHTS; // Simulate load from Web service
+  }  
 }
