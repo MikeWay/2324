583b290e0a64f4c0d7c5ce46a89acac29b55ed3f
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2024-08-09 13:22:12 +0100

    Updated Ex/FlySharp to start

--- Exercises/FlySharp/src/app/app.component.spec.ts
+++ Exercises/FlySharp/src/app/app.component.spec.ts
@@ -1,25 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { provideRouter } from '@angular/router';
-import { ApplicationStateService } from './application-state/application-state.service';
-import { FLIGHTS, MYFLIGHTS } from './model/mock-flights';
 
 describe('AppComponent', () => {
-
-  const spyApplicationStateService = jasmine.createSpyObj<ApplicationStateService>('MockApplicationStateService', [],
-    {
-        flights: FLIGHTS,
-        myFlights: MYFLIGHTS,
-        displayCurrency: { code: 'GBP', symbol: '£', rate: 1.0 }
-    });
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers:[provideRouter([])]
-    }).overrideComponent(AppComponent,
-      { set: { providers: [{ provide: ApplicationStateService, useValue: spyApplicationStateService }] } })
-      .compileComponents();
+    }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -28,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Fly Sharp' title`, () => {
+  it(`should have the 'FlySharp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Fly Sharp');
+    expect(app.title).toEqual('FlySharp');
   });
 
-  it('should have a <router-outlet>', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('router-outlet')).toBeDefined();
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, FlySharp');
   });
 });
--- Exercises/FlySharp/src/app/app.component.ts
+++ Exercises/FlySharp/src/app/app.component.ts
@@ -1,33 +1,13 @@
 import { Component } from '@angular/core';
-import { NgClass } from '@angular/common'
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { BuyFlightComponent } from './buy-flight/buy-flight.component';
-import { CurrencySelectorComponent } from './currency-selector/currency-selector.component';
-import { TimeDirective } from './time/time.directive';
-
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, BuyFlightComponent, NgClass, RouterLink, CurrencySelectorComponent, TimeDirective,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule,
-    MatIconModule
-  ],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {    
-  title = 'Fly Sharp';
-  navbarOpen = false;
-
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
+export class AppComponent {
+  title = 'FlySharp';
 }
