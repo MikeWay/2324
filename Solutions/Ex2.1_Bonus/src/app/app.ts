c5f489f98751cfe78223376d47bba2f06b2b0739
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2026-04-15 13:30:15 +0100

    Refactor Ex2.1 Bonus: Update app structure, remove unused components, and enhance app configuration

---
+++ Solutions/Ex2.1_Bonus/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, HomeComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('Fly Sharp');
+}
