88996ec2d6c4eadaeb896bccad69dfa683677782
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2026-04-15 14:29:48 +0100

    Start Point Ex3.1

--- Exercises/FlySharp/src/app/app.ts
+++ Exercises/FlySharp/src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home';
+import { BuyFlight } from './buy-flight/buy-flight';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, BuyFlight, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Fly Sharp');
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
