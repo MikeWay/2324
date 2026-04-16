30a24cc793c1d1fa92492dea91b074d0f2717861
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2026-04-16 14:44:34 +0100

    Ex5.2 and 5.3 done

--- Solutions/Ex5.2/src/app/app.routes.ts
+++ Solutions/Ex5.2/src/app/app.routes.ts
@@ -1,45 +1,15 @@
 import { Routes } from '@angular/router';
-import { AccountComponent } from './account/account.component';
-import { BuyFlightComponent } from './buy-flight/buy-flight.component';
-import { HomeComponent } from './home/home.component';
-import { MyFlightsComponent } from './my-flights/my-flights.component';
+import { HomeComponent } from './home/home';
+import { BuyFlight } from './buy-flight/buy-flight';
+import { MyFlights } from './my-flights/my-flights';
 
 export const routes: Routes = [
-    {
-        path: '',
-        redirectTo: '/home',
-        pathMatch: 'full'
-    },
-    {
-        path: 'home',
-        component: HomeComponent
-    },
-    {
-        path: 'buy',
-        component: BuyFlightComponent
-    },
-    {
-        path: 'buy/:origin',
-        component: BuyFlightComponent
-    },    
-    {
-        path: 'buy/:origin/:destination',
-        component: BuyFlightComponent
-    },     
-    {
-        path: 'myflights',
-        component: MyFlightsComponent
-    },
-    {
-        path: 'account',
-        loadChildren: () => import('./accounts/accounts.module').then(mod => mod.AccountsModule)
-    },
-
-    {
-
-        path: '**',
-        component: HomeComponent
-    },
-
-
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'buy', component: BuyFlight },
+  { path: 'buy/:origin', component: BuyFlight },
+  { path: 'buy/:origin/:destination', component: BuyFlight },
+  { path: 'myflights', component: MyFlights },
+  { path: 'account', loadChildren: () => import('./accounts/accounts-module').then(mod => mod.AccountsModule) },
+  { path: '**', component: HomeComponent }
 ];
