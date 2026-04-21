8798c8dbc76f522b57b0b59840eaf9d76575eecf
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2026-04-21 10:17:02 +0100

    Ex4.1 Reworked

--- Solutions/Ex4.1/src/app/buy-flight/buy-flight.ts
+++ Solutions/Ex4.1/src/app/buy-flight/buy-flight.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ApplicationState } from '../application-state/application-state';
-import { Flight } from '../model/flight';
 import { Payment } from '../payment/payment';
+import { Flight } from '../model/flight';
 
 @Component({
   selector: 'app-buy-flight',
@@ -10,20 +10,19 @@ import { Payment } from '../payment/payment';
   styleUrl: './buy-flight.scss',
 })
 export class BuyFlight {
+  private stateService = inject(ApplicationState);
   showBuyFlights = true;
   selectedFlight: Flight | undefined;
 
-  constructor(private stateService: ApplicationState) {}
-
-  get flights(): Flight[] {
+  get flights() {
     return this.stateService.flights;
-  }
-
-  onFlightClick(flight: Flight): void {
-    this.selectedFlight = flight;
   }
 
   onClickBuyFlights() {
     this.showBuyFlights = !this.showBuyFlights;
+  }
+
+  onFlightClick(flight: Flight) {
+    this.selectedFlight = flight;
   }
 }
