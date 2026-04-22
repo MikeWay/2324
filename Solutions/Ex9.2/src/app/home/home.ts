38e876e479ef404485a6b782b2e4c08c158f280a
Mike Way <712020:1ff1c79b-0f02-4f01-9a83-b7ae01b491fa@bots.bitbucket.org>
2026-04-22 17:08:57 +0100

    Refactor Home component imports and update string quotes for consistency

--- Solutions/Ex9.2/src/app/home/home.ts
+++ Solutions/Ex9.2/src/app/home/home.ts
@@ -1,12 +1,12 @@
-import { Component } from "@angular/core";
-import { FlightStatusComponent } from "../flight-status/flight-status.component";
+import { Component } from '@angular/core';
+import { FlightStatus } from '../flight-status/flight-status';
 
 @Component({
   selector: 'app-home',
-    standalone: true,
-    imports: [FlightStatusComponent],
-    template: `<h1>Special Offer of the month {{specialOffer}}</h1><app-flight-status/>`,
+  standalone: true,
+  imports: [FlightStatus],
+  template: `<h1>Special Offer of the month {{specialOffer}}</h1><app-flight-status/>`
 })
 export class Home {
-  specialOffer = '10% off all round-the-World flights';
+  specialOffer = "10% off all round-the-World flights";
 }
